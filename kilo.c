c7c6e262a9df0630487b3a5aed20aa73b1ce9c11
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2017-04-16 13:31:29 -0700

    Added miscellaneous flags
    * Most of these are already turned off in modern terminals

--- kilo.c
+++ kilo.c
@@ -14,9 +14,10 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-	raw.c_iflag &= ~(IXON | ICRNL);
-	raw.c_oflag &= ~(OPOST);
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
