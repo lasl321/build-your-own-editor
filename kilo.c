5305d2593b8fa19c3441a970268b4335878065cd
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2017-04-16 13:28:00 -0700

    Turned off output processing
    * Has to do with automatically adding a carriage return to newlines

--- kilo.c
+++ kilo.c
@@ -14,8 +14,9 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-	raw.c_iflag &= ~(IXON);
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -25,9 +26,9 @@ int main() {
 	char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
 	return 0;
