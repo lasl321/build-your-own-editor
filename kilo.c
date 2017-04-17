9f3ae2875196df0956aae9ba76d1d2e0f6654e2b
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2017-04-16 17:03:22 -0700

    Added code to clear screen on error

--- kilo.c
+++ kilo.c
@@ -9,6 +9,9 @@
 
 void die(const char *s)
 {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -65,6 +68,7 @@ void enableRawMode()
 void editorRefreshScreen()
 {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 int main()
